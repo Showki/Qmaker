4e3e4a659660574560a757741a9d2d554abc1bde
fksk <ering.scissorman@gmail.com>
2015-09-26 09:25:38 +0900

    知識ベースアクセスのためのModel作成

---
+++ app/Model/Knowledge.php
@@ -0,0 +1,10 @@
+<?php
+class Knowledge extends AppModel{
+	public $name = 'Knowledge';
+	// public $belongsTo = array(
+	// 	'Category' => array(
+	// 		'className' => 'Category',
+	// 		'foreignKey' => 'category_id'
+	// 		),
+}
+?>
